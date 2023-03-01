97a44226eeb1fdb1ec951b48e2231be55b988417
roger.santos <roger.rezende@gmail.com>
2023-03-01 17:16:52 -0300

    Criação do teste automatizado para acesso das telas

---
+++ cypress/integration/visit.spec.js
@@ -0,0 +1,22 @@
+it('Acessar a tela de home', () => {
+    cy.visit('/')
+    cy.url().should('be.equal', 'https://automationexercise.com/')
+})
+
+it('Acessar a tela de products', () => {
+    cy.visit('/')
+    cy.get('a[href*="/products"]').click()
+    cy.url().should('be.equal', 'https://automationexercise.com/products')
+})
+
+it.only('Acessar a tela de cart', () => {
+    cy.visit('/')
+    cy.get('.shop-menu > .nav > :nth-child(3) > a').click()
+    cy.url().should('be.equal', 'https://automationexercise.com/view_cart')
+})
+
+it('Acessar a tela de signup/login', () => {
+    cy.visit('/')
+    cy.get('a[href*="/login"]').click()
+    cy.url().should('be.equal', 'https://automationexercise.com/login')
+})
